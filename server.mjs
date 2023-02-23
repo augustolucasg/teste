a90c563addc59c1f8d83e85d22f9f6b34c520e9b
augustolucasg <98846129+augustolucasg@users.noreply.github.com>
2023-02-23 11:53:55 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,14 @@
 import { createServer } from 'http';
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://app.atendezap.net/30382/broadcast');
   res.end();
+  
+  
+  
 }).listen(process.env.PORT);
